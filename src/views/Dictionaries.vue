9c00436dc0bce35bbfb8c6eef0922fd14fac2083
aleksandr <ugolkov.prog@gmail.com>
2024-01-20 13:54:11 +0200

    fix dictionary page

--- src/views/Dictionaries.vue
+++ src/views/Dictionaries.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HeaderPanel from '@/components/HeaderPanel.vue';
 import DictionariesList from '@/components/DictionariesList.vue';
+import LoadingPage from '@/components/LoadingPage.vue';
 </script>
 
 <template>
